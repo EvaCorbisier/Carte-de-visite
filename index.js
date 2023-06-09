b15fe912aed4d0142db0dce683424a820512302e
EvaCorbisier <evacorbisier@gmail.com>
2023-06-09 12:54:19 +0200

    V12

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
-// Créer variables des boutons Voix et Musique
-
-var boutonMusique =  document.querySelector('#boutonmusique')
-var boutonVoix =  document.querySelector('#boutonvoix')
-
-
-// Créer variables de chaque vidéo et voix
+// Créer variables de chaque vidéo
 
 var showreel = document.querySelector('#showreel')
 
-voixCouverture.volume = 0.5;
 
-// Changer l'aspect des boutons quand click
-//Passer de 0.4 à 1 d'opacité les 2 premiers boutons et mute/unmute les vidéos et les voix contées
-
-function musiqueOnOff() {
-    if (boutonMusique.style.opacity == 0.4){
-        boutonMusique.style.opacity = 1;
-        vidCouverture.muted = false;
-    }
-    else {
-        boutonMusique.style.opacity = 0.4;
-        vidCouverture.muted = true;
-    }
-}
-
-
-                    // COUVERTURE
-
+                    // SHOWREEL
 
 // ressources de création d'évènement pour la partie AR, target found etc, source : MindAR
 
@@ -36,22 +13,23 @@ document.addEventListener("DOMContentLoaded", function() {
 	
 	// arReady event triggered when ready
 	sceneEl.addEventListener("arReady", (event) => {
-	  // console.log("MindAR is ready")
+	  console.log("MindAR is ready")
 	});
 	// arError event triggered when something went wrong. Mostly browser compatbility issue
 	sceneEl.addEventListener("arError", (event) => {
-	  // console.log("MindAR failed to start")
+	  console.log("MindAR failed to start")
 	});
 
 	// detect target found
 	targetCarte.addEventListener("targetFound", event => {
 	  console.log("target found");
-      showreel.play();
+    showreel.play();
 	});
 
 	// detect target lost
 	targetCarte.addEventListener("targetLost", event => {
 	  console.log("target lost");
-      showreel.pause();
-    });
+    showreel.pause();
+ });
+ 
 });
